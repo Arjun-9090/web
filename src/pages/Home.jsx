e93fb1fd8cf6b4a8f8e17060d7087a6fdcc1e51d
Arjun-9090 <ww.arjunstha@gmail.com>
2024-12-27 07:22:53 +0545

    somes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,41 +1,37 @@
-import { Button, Typography } from "@material-tailwind/react";
-import { useState } from "react";
+import { TimelineCompo } from "../components/TimelineCompo"
 
+const persons = [
+  {
+    name: 'John Doe',
+    age: 25,
+    profession: 'Web Developer'
+  },
+  {
+    name: 'Jane Doe',
+    age: 22,
+    profession: 'Web Designer'
+  },
+  {
+    name: 'Jack Doe',
+    age: 30,
+    profession: 'Software Engineer'
+  }
+]
 
 const Home = () => {
 
 
 
-  const [count, setCount] = useState(0);
-
-  const handleIncre = () => {
-    setCount((prev) => prev + 1);
-    console.log(count);
-  }
-  const om = () => {
-    setCount((prev) => prev - 1);
-
-  }
-
   return (
-    <div className="p-4">
-      <div className=" max-w-[200px] flex flex-col items-center space-y-3">
+    <div className="p-5">
 
-        <div className="flex items-center gap-3">
-          <Typography className="text-center" variant="h3">{count}</Typography>
-          <Typography>
-            {count !== 0 && `${count % 2 == 0 ? `Even Number` : 'Odd Number'}`}</Typography>
-        </div>
-        <Button
-          onClick={handleIncre}
-          size="sm" color="green">Increment</Button>
 
-        <Button
-          disabled={count === 0}
-          onClick={om}
-          size="sm" color="pink">Decrement</Button>
+      <TimelineCompo persons={persons} />
 
-      </div>
+
+
+
+
     </div>
   )
 }
