59dd1632aeb0d72035e47e5efd17b16f275817e8
Arjun-9090 <ww.arjunstha@gmail.com>
2024-12-27 15:42:23 +0545

    somes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,39 @@
-import { TimelineCompo } from "../components/TimelineCompo"
-
-const persons = [
-  {
-    name: 'John Doe',
-    age: 25,
-    profession: 'Web Developer'
-  },
-  {
-    name: 'Jane Doe',
-    age: 22,
-    profession: 'Web Designer'
-  },
-  {
-    name: 'Jack Doe',
-    age: 30,
-    profession: 'Software Engineer'
-  }
-]
+import { faker } from "@faker-js/faker";
+import { Button } from "@material-tailwind/react";
+import { useState } from "react";
+import { TableCompo } from "../components/TableCompo";
 
 const Home = () => {
 
+  const [users, setUser] = useState([]);
+
+  // const nis = [9, 9];
+  // console.log([...nis, 90]);
+
+  const addUser = () => {
+    const newUser = {
+      userId: faker.string.uuid(),
+      name: faker.internet.displayName(),
+      avatar: faker.image.avatarGitHub(),
+      bio: faker.person.bio()
+    };
+
+    setUser((prev) => [...prev, newUser]);
+
+  }
 
 
   return (
-    <div className="p-5">
+    <div className="p-5 space-y-5">
 
+      <Button
+        onClick={addUser}
+        size="sm" color="green" ripple>Add Post</Button>
 
-      <TimelineCompo persons={persons} />
-
-
+      {/* {users.map(({ userId, name }) => {
+        return <h1>{name}</h1>
+      })} */}
+      {users.length > 0 && <TableCompo persons={users} />}
 
 
 
