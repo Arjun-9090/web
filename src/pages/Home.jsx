f273e02b9b4393a9ea45d1e3c92ac95d9308af7b
Arjun-9090 <ww.arjunstha@gmail.com>
2024-12-29 18:14:49 +0545

    somes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,12 @@
-import { faker } from "@faker-js/faker";
-import { Button } from "@material-tailwind/react";
+import { fa, faker } from "@faker-js/faker";
+import { Button, IconButton } from "@material-tailwind/react";
 import { useState } from "react";
-import { TableCompo } from "../components/TableCompo";
+import AppDialog from "../components/AppDialog";
 
 const Home = () => {
 
   const [users, setUser] = useState([]);
 
-  // const nis = [9, 9];
-  // console.log([...nis, 90]);
 
   const addUser = () => {
     const newUser = {
@@ -17,25 +15,30 @@ const Home = () => {
       avatar: faker.image.avatarGitHub(),
       bio: faker.person.bio()
     };
-
     setUser((prev) => [...prev, newUser]);
-
+  }
+  const removeUser = (index) => {
+    users.splice(index, 1);
+    setUser((prev) => [...prev]);
   }
 
 
   return (
-    <div className="p-5 space-y-5">
+    <div className="p-5 space-y-4">
 
       <Button
         onClick={addUser}
         size="sm" color="green" ripple>Add Post</Button>
 
-      {/* {users.map(({ userId, name }) => {
-        return <h1>{name}</h1>
-      })} */}
-      {users.length > 0 && <TableCompo persons={users} />}
+
+      {users.map((user, i) => {
+        return <div className="flex gap-4 items-center" key={user.userId}>
+          <h1 className="text-xl">{user.bio}</h1>
+          <AppDialog removeUser={removeUser} index={i} />
 
 
+        </div>
+      })}
 
     </div>
   )
