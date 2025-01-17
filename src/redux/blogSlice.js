405d463138df885698464efea04b0140c85be408
Arjun-9090 <ww.arjunstha@gmail.com>
2025-01-17 07:44:34 +0545

    somes

---
+++ src/redux/blogSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const blogSlice = createSlice({
+  name: 'blogSlice',
+  initialState: {
+    blogs: []
+  },
+  reducers: {
+    addBlog: (state, action) => {
+      state.blogs.push(action.playload);
+    }
+  }
+});
